f3026d69893270a088a4dbefbfe281a4ceada26f
FirankisonB <bmindaye12@gmail.com>
2023-01-11 13:02:53 -0800

    T7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     if key not in a_dictionary:
-        a_dictionary[key] = 
+        a_dictionary[key] =  value
     else:
         for i in a_dictionary:
             if i == key:
