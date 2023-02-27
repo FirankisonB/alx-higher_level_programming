ac26474ece26ebe3116d324188f766d0eb2ad2e0
FirankisonB <bmindaye12@gmail.com>
2023-02-27 15:30:06 -0800

    python classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# # Mindaye Bogale Defaru <bmindaye12@gmail.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
