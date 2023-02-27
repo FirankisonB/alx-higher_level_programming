1adbc32ddd52e0b3e0e259d3fcaa47d8f6c1a09a
FirankisonB <bmindaye12@gmail.com>
2023-02-27 14:08:33 -0800

    python expections

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Mindaye Bogale Defaru <bmindaye12@gmail.com>
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
