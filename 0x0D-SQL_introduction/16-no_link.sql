389a7b12a75348bc36a98eb1d2442f8fe24925f1
FirankisonB <bmindaye12@gmail.com>
2023-03-14 09:22:27 -0700

    t16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,6 @@
--- lists all records of the second_table
-SELECT score, name FROM second_table WHERE NOT name='' ORDER BY score DESC;
+-- Lists all records of the table second_table having a name value.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
